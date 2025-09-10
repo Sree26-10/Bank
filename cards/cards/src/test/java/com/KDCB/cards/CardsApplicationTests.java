d9b02d1c4ef12e90b8168780a7e53e00d958e068
Sree26-10 <sreehari.kammari@gmail.com>
2025-09-10 13:09:41 +0530

    Initial commit

---
+++ cards/cards/src/test/java/com/KDCB/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KDCB.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
